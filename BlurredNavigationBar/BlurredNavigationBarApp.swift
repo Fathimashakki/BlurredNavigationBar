989e613f83aaa482e60d8b064c53306b70e29ca7
Fathima Shakki K C <fathimashakki21@gmail.com>
2025-05-23 12:31:16 +0530

    Initial Commit

---
+++ BlurredNavigationBar/BlurredNavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurredNavigationBarApp.swift
+//  BlurredNavigationBar
+//
+//  Created by Shaan on 23/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BlurredNavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
